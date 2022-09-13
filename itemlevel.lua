61713db19ba252f9e2cc40c9a1bec5c7641d32c1
Pingumania <pingumania@users.noreply.github.com>
2022-09-13 22:00:14 +0200

    Show item level on Legion tokens

--- itemlevel.lua
+++ itemlevel.lua
@@ -5,8 +5,8 @@ local type, tonumber, select, strsplit, GetItemInfoFromHyperlink = type, tonumbe
 local unpack, GetDetailedItemLevelInfo = unpack, GetDetailedItemLevelInfo
 
 do
-    local oGetItemInfo = GetItemInfo
-    ns.itemcache = ns.itemcache or setmetatable({miss = 0, tot = 0}, {__index = function(table, key)
+	local oGetItemInfo = GetItemInfo
+	ns.itemcache = ns.itemcache or setmetatable({miss = 0, tot = 0}, {__index = function(table, key)
 			if not key then return "" end
 			if key == "miss" then return 0 end
 			if key == "tot" then return 0 end
@@ -44,93 +44,93 @@ local itemLevelPattern = _G.ITEM_LEVEL:gsub('%%d', '(%%d+).?%%(?(%%d*)%%)?')
 local minItemLevelPattern = _G.ITEM_LEVEL:gsub('%%d', '(%%d+%%+?).?%%(?(%%d*)%%)?')
 
 local function ScanTip(itemLink, id, slot)
-    if type(itemLink) == "number" then
+	if type(itemLink) == "number" then
 		itemLink = CachedGetItemInfo(itemLink, 2)
 		if not itemLink then return emptytable end
 	end
-    
-    if type(tipCache[itemLink].gearcheck) == "nil" then
-        local cacheIt = true
-        
-        tipCache[itemLink] = {
-            ilevel = nil,
-            ilevelString = nil,
-            quality = nil,
-            gearcheck = nil,
-            cached = cacheIt
-        }
-        local c = tipCache[itemLink]
-        c.gearcheck = SyLevel:CheckGear(itemLink)
-    end
-    
-    if tipCache[itemLink].gearcheck == false then
-        return emptytable
-    end
+	
+	if type(tipCache[itemLink].gearcheck) == "nil" then
+		local cacheIt = true
+		
+		tipCache[itemLink] = {
+			ilevel = nil,
+			ilevelString = nil,
+			quality = nil,
+			gearcheck = nil,
+			cached = cacheIt
+		}
+		local c = tipCache[itemLink]
+		c.gearcheck = SyLevel:CheckGear(itemLink)
+	end
+	
+	if tipCache[itemLink].gearcheck == false then
+		return emptytable
+	end
 
 	if type(tipCache[itemLink].ilevel) == "nil" then
-        local cacheIt = true
-        local skipScan = nil
-        local _, quality
-                     
+		local cacheIt = true
+		local skipScan = nil
+		local _, quality
+					 
 		if not scanningTooltip then
 			scanningTooltip = _G.CreateFrame("GameTooltip", "GearLevelScanTooltip", nil, "GameTooltipTemplate")
-            anchor = CreateFrame("Frame")
+			anchor = CreateFrame("Frame")
 			anchor:Hide()
 		end
-        GameTooltip_SetDefaultAnchor(scanningTooltip, anchor)
-        scanningTooltip:ClearLines()
+		GameTooltip_SetDefaultAnchor(scanningTooltip, anchor)
+		scanningTooltip:ClearLines()
 		local itemString = itemLink:match("|H(.-)|h")
 		local rc, message = pcall(scanningTooltip.SetHyperlink, scanningTooltip, itemString)
-        if id and type(id) == "string" and slot then
-            rc, message = pcall(scanningTooltip.SetInventoryItem, scanningTooltip, id, slot, nil, true)
-            quality = GetInventoryItemQuality(id, slot)
-        elseif id and type(id) == "number" and slot then
-            rc, message = pcall(scanningTooltip.SetBagItem, scanningTooltip, id, slot) 
-            _, _, _, quality = GetContainerItemInfo(id, slot)
-        elseif id then
-            rc = true
-            skipScan = true
-            quality = id:GetItemQuality()
-        else
-            _, _, quality = GetItemInfo(itemLink)
-        end
+		if id and type(id) == "string" and slot then
+			rc, message = pcall(scanningTooltip.SetInventoryItem, scanningTooltip, id, slot, nil, true)
+			quality = GetInventoryItemQuality(id, slot)
+		elseif id and type(id) == "number" and slot then
+			rc, message = pcall(scanningTooltip.SetBagItem, scanningTooltip, id, slot) 
+			_, _, _, quality = GetContainerItemInfo(id, slot)
+		elseif id then
+			rc = true
+			skipScan = true
+			quality = id:GetItemQuality()
+		else
+			_, _, quality = GetItemInfo(itemLink)
+		end
 		if not rc then return emptytable end
-      	
-        local c = tipCache[itemLink]
-        if skipScan then
-            c.ilevel = id:GetCurrentItemLevel()
-            c.ilevelString = id:GetCurrentItemLevel()
-        else
-            for i = 2, 3 do
-                local label, text = _G["GearLevelScanTooltipTextLeft"..i], nil
-                if label then text = label:GetText() end
-                if text then
-                    local normal, timewalking
-                    if c.ilevel == nil then
-                        normal, timewalking = text:match(itemLevelPattern)
-                        if timewalking ~= "" then
-                            c.ilevel = tonumber(timewalking)
-                        else
-                            c.ilevel = tonumber(normal)
-                        end
-                    end
-                    if c.ilevelString == nil then
-                        normal, timewalking = text:match(minItemLevelPattern)
-                        if timewalking ~= "" then
-                            c.ilevelString = timewalking
-                        else
-                            c.ilevelString = normal
-                        end
-                    end
-                end
-            end
-        end
-        
-        c.ilevel = c.ilevel or 1
-        c.quality = quality
-        scanningTooltip:Hide()
+		  
+		local c = tipCache[itemLink]
+		if skipScan then
+			c.ilevel = id:GetCurrentItemLevel()
+			c.ilevelString = id:GetCurrentItemLevel()
+		else
+			for i = 2, 3 do
+				local label, text = _G["GearLevelScanTooltipTextLeft"..i], nil
+				if label then text = label:GetText() end
+				if text then
+					local normal, timewalking
+					if c.ilevel == nil then
+						normal, timewalking = text:match(itemLevelPattern)
+						if timewalking ~= "" then
+							c.ilevel = tonumber(timewalking)
+						else
+							c.ilevel = tonumber(normal)
+						end
+					end
+					if c.ilevelString == nil then
+						normal, timewalking = text:match(minItemLevelPattern)
+						if timewalking ~= "" then
+							c.ilevelString = timewalking
+						else
+							c.ilevelString = normal
+						end
+					end
+				end
+			end
+		end
+		
+		c.ilevel = c.ilevel or 1
+		c.quality = quality
+		scanningTooltip:Hide()
 	end
-    
+	
 	return tipCache[itemLink]
 end
 
@@ -144,22 +144,22 @@ function SyLevel:GetHeirloomTrueLevel(itemString, id, slot)
 	if rc.ilevel and rc.ilevelString then
 		return rc.ilevel, rc.ilevelString, rc.quality, true
 	else
-        return nil, false
-    end
+		return nil, false
+	end
 end
 
 function SyLevel:GetUpgradedItemLevel(itemString, id, slot)
 	local ilvl, ilvlText, quality, isTrue = self:GetHeirloomTrueLevel(itemString, id, slot)
-    if isTrue then
-        return ilvl, ilvlText, quality
-    end
+	if isTrue then
+		return ilvl, ilvlText, quality
+	end
 end
 
 function SyLevel:CheckGear(itemString)
-    local _, _, _, _, _, itemClass, itemSubClass = GetItemInfoInstant(itemString)
-    if itemClass == LE_ITEM_CLASS_WEAPON or itemClass == LE_ITEM_CLASS_ARMOR or itemSubClass == LE_ITEM_ARMOR_RELIC then
-        return true 
-    else 
-        return false
-    end
+	local _, _, _, _, _, itemClass, itemSubClass = GetItemInfoInstant(itemString)
+	if itemClass == LE_ITEM_CLASS_WEAPON or itemClass == LE_ITEM_CLASS_ARMOR or itemSubClass == LE_ITEM_ARMOR_RELIC or itemSubClass == LE_ITEM_ARMOR_IDOL then
+		return true 
+	else 
+		return false
+	end
 end
