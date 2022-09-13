aa5cb291eaaceb4737414d5f5af744cb91652f55
Pingumania <pingumania@users.noreply.github.com>
2022-09-13 22:00:18 +0200

    Don't show itemlevel on reagents

--- pipes/tradeskill.lua
+++ pipes/tradeskill.lua
@@ -11,12 +11,12 @@ local function update(self)
 	local itemLink = C_TradeSkillUI.GetRecipeItemLink(selectedRecipeID)
 	SyLevel:CallFilters("tradeskill", TradeSkillFrame.DetailsFrame.Contents.ResultIcon, _E and itemLink)
 
-	local numReagents = C_TradeSkillUI.GetRecipeNumReagents(selectedRecipeID)
-	for reagentIndex = 1, numReagents do
-		local reagentFrame = TradeSkillFrame.DetailsFrame.Contents.Reagents[reagentIndex]
-		local reagentLink = C_TradeSkillUI.GetRecipeReagentItemLink(selectedRecipeID, reagentIndex)
-		SyLevel:CallFilters("tradeskill", reagentFrame, _E and reagentLink)
-	end
+	-- local numReagents = C_TradeSkillUI.GetRecipeNumReagents(selectedRecipeID)
+	-- for reagentIndex = 1, numReagents do
+	-- 	local reagentFrame = TradeSkillFrame.DetailsFrame.Contents.Reagents[reagentIndex]
+	-- 	local reagentLink = C_TradeSkillUI.GetRecipeReagentItemLink(selectedRecipeID, reagentIndex)
+	-- 	SyLevel:CallFilters("tradeskill", reagentFrame, _E and reagentLink)
+	-- end
 end
 
 local function doHook()
