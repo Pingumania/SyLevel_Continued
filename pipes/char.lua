892f6b44c21cc5f3cccdbdb992212082f20dd54b
Pingumania <pingumania@users.noreply.github.com>
2022-09-13 22:00:13 +0200

    Update char pipe

--- pipes/char.lua
+++ pipes/char.lua
@@ -1,8 +1,6 @@
 -- TODO:
 --  - Write a description.
 
-if (select(4, GetAddOnInfo("Fizzle"))) then return end
-
 local _E
 local hook
 local slots = {
@@ -13,46 +11,52 @@ local slots = {
 
 CharacterNeckSlot.RankFrame:SetAlpha(0)
 
-local function update(self)
-	if (CharacterFrame:IsShown()) then
+local function update(self, key, slotFrame)
+	item = Item:CreateFromEquipmentSlot(key)
+	itemLoc = ItemLocation:CreateFromEquipmentSlot(key)
+	if C_Item.DoesItemExist(itemLoc) then
+		slotLink = item:GetItemLink()
+		SyLevel:CallFilters("char", slotFrame, _E and slotLink, item)
+	else
+		SyLevel:CallFilters("char", slotFrame, _E and nil)
+	end
+end
+
+local function pipe(self, slot)
+	local slotFrame
+	if slot then
+		slotFrame = _G["Character"..slots[slot].."Slot"]
+		update(self, slot, slotFrame)
+	else
 		for key, slotName in pairs(slots) do
-			local slotFrame = _G["Character"..slotName.."Slot"]
-			local item = Item:CreateFromEquipmentSlot(key)
-			local itemLoc = ItemLocation:CreateFromEquipmentSlot(key)
-			if C_Item.DoesItemExist(itemLoc) then
-				local slotLink = item:GetItemLink()
-				SyLevel:CallFilters("char", slotFrame, _E and slotLink, item)
-			else
-				SyLevel:CallFilters("char", slotFrame, _E and nil)
-			end
+			slotFrame = _G["Character"..slotName.."Slot"]
+			update(self, key, slotFrame)
 		end
 	end
 end
 
-local function UNIT_INVENTORY_CHANGED(self, event, unit)
-	if (unit == "player") then
-		update(self)
-	end
+local function PLAYER_EQUIPMENT_CHANGED(self, event, slot)
+	pipe(self, slot)
 end
 
 local function doHook()
     if (not hook) then
 		hook = function(...)
-			if (_E) then return update(...) end
+			if (_E) then return pipe(...) end
 		end
-		CharacterFrame:HookScript("OnShow", update)
+		CharacterFrame:HookScript("OnShow", pipe)
 	end
 end
 
 local function enable(self)
 	_E = true
-	self:RegisterEvent("UNIT_INVENTORY_CHANGED", UNIT_INVENTORY_CHANGED)
+	self:RegisterEvent("PLAYER_EQUIPMENT_CHANGED", PLAYER_EQUIPMENT_CHANGED)
 	doHook()
 end
 
 local function disable(self)
 	_E = nil
-	self:UnregisterEvent("UNIT_INVENTORY_CHANGED", UNIT_INVENTORY_CHANGED)
+	self:UnregisterEvent("PLAYER_EQUIPMENT_CHANGED", PLAYER_EQUIPMENT_CHANGED)
 end
 
 SyLevel:RegisterPipe("char", enable, disable, update, "Character", nil)
