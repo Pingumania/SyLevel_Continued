2f9526306cb903b5f281893a535e78cd1a636738
Pingumania <pingumania@users.noreply.github.com>
2022-11-22 02:41:11 +0100

    Fix professions

--- pipes/tradeskill.lua
+++ pipes/tradeskill.lua
@@ -1,22 +1,8 @@
 local _E
 local hook
-local selectedRecipeID
 
-local function update(self)
-	if (not TradeSkillFrame:IsVisible()) then return end
-	local recipeID = self.RecipeList:GetSelectedRecipeID()
-	selectedRecipeID = recipeID and recipeID or nil
-	if not selectedRecipeID then return end
-
-	local itemLink = C_TradeSkillUI.GetRecipeItemLink(selectedRecipeID)
-	SyLevel:CallFilters("tradeskill", TradeSkillFrame.DetailsFrame.Contents.ResultIcon, _E and itemLink)
-
-	-- local numReagents = C_TradeSkillUI.GetRecipeNumReagents(selectedRecipeID)
-	-- for reagentIndex = 1, numReagents do
-	-- 	local reagentFrame = TradeSkillFrame.DetailsFrame.Contents.Reagents[reagentIndex]
-	-- 	local reagentLink = C_TradeSkillUI.GetRecipeReagentItemLink(selectedRecipeID, reagentIndex)
-	-- 	SyLevel:CallFilters("tradeskill", reagentFrame, _E and reagentLink)
-	-- end
+local function update(outputIcon, _, _, _, itemIDOrLink)
+	SyLevel:CallFilters("tradeskill", outputIcon, _E and itemIDOrLink)
 end
 
 local function doHook()
@@ -24,13 +10,12 @@ local function doHook()
 		hook = function(...)
 			if (_E) then return update(...) end
 		end
-
-		hooksecurefunc(TradeSkillFrame, "OnRecipeChanged", hook)
+		hooksecurefunc(Professions, "SetupOutputIconCommon", hook)
 	end
 end
 
 local function ADDON_LOADED(self, event, addon)
-	if (addon == "Blizzard_TradeSkillUI") then
+	if (addon == "Blizzard_Professions") then
 		doHook()
 		self:UnregisterEvent(event, ADDON_LOADED)
 	end
@@ -39,7 +24,7 @@ end
 local function enable(self)
 	_E = true
 
-	if (IsAddOnLoaded("Blizzard_TradeSkillUI")) then
+	if (IsAddOnLoaded("Blizzard_Professions")) then
 		doHook()
 	else
 		self:RegisterEvent("ADDON_LOADED", ADDON_LOADED)
